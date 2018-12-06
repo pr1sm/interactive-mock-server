5db499c7a6c70198fef7ba1a9f5539c86cdba1f8
Srinivas Dhanwada <dhanwada.dev@gmail.com>
2018-12-06 16:16:17 -0600

    Add Logs Component
    This commit adjusts the gitignore to allow the Logs component in the
    dashboard folder not be ignored.

---
+++ web/logs/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Logs = () => (
+  <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+    <h1 className="h2">Logs</h1>
+  </div>
+);
+
+export default Logs;
