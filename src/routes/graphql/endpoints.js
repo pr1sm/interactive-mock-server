acadbba92fa85f061c9dddfa80d1b79cd25c85ba
Srinivas Dhanwada <dhanwada.dev@gmail.com>
2018-12-11 03:27:46 -0600

    Finish Implementing Endpoint GraphQL Schema
    This commit completes the rest of the graphQL Schema up to a working point. There are some validation checks (mostly non-null and some like type checking), but more checks are necessary (condition requirement of redirect, etc.). 
    
    The mime type enum is updated to include JSON and the correct value for JS. An additional Http method was added to hold the method for the endpoint.
    
    the API has been split up correctly between the endpoint query and endpoint mutation objects. These methods simply rely on the root object, which is the store given to us in the setup function
    
    The main API setup function has been updated to add the GraphQL route. The GraphiQL interface is also enabled for testing (should be hidden around a dev env flag).

--- src/routes/graphql/endpoints.js
+++ src/routes/graphql/endpoints.js
@@ -1,4 +1,5 @@
 const graphql = require('graphql');
+const graphqlHTTP = require('express-graphql');
 
 const {
   GraphQLEnumType,
@@ -6,19 +7,16 @@ const {
   GraphQLList,
   GraphQLNonNull,
   GraphQLObjectType,
-  GraphQLQueryType,
+  GraphQLSchema,
   GraphQLString,
 } = graphql;
 
 // MIME Type Enum Definition
 const mimeTypeEnum = new GraphQLEnumType({
   name: 'MimeType',
+  type: GraphQLString,
   description: 'The type of a mock endpoint response',
   values: {
-    TEXT: {
-      value: 'text/plain',
-      description: 'Default Text',
-    },
     PLAIN: {
       value: 'text/plain',
       description: 'Plain Text',
@@ -27,13 +25,53 @@ const mimeTypeEnum = new GraphQLEnumType({
       value: 'text/html',
       description: 'HTML Markup',
     },
-    JAVASCRIPT: {
-      value: 'text/javascript',
-      description: 'Javascript',
-    },
     CSS: {
       value: 'text/css',
       description: 'Cascading Style Sheet',
+    },
+    JAVASCRIPT: {
+      value: 'application/javascript',
+      description: 'Javascript',
+    },
+    JSON: {
+      value: 'application/json',
+      description: 'JSON Object',
+    },
+  },
+});
+
+// HTTP Method Enum Definition
+const httpMethodEnum = new GraphQLEnumType({
+  name: 'HttpMethod',
+  type: GraphQLString,
+  description: 'An HTTP method',
+  values: {
+    GET: {
+      description: 'GET method',
+    },
+    HEAD: {
+      description: 'HEAD method',
+    },
+    POST: {
+      description: 'POST method',
+    },
+    PUT: {
+      description: 'PUT method',
+    },
+    DELETE: {
+      description: 'DELETE method',
+    },
+    CONNECT: {
+      description: 'CONNECT method',
+    },
+    OPTIONS: {
+      description: 'OPTIONS method',
+    },
+    TRACE: {
+      description: 'TRACE method',
+    },
+    PATCH: {
+      description: 'PATCH method',
     },
   },
 });
@@ -44,7 +82,7 @@ const endpointResponseType = new GraphQLObjectType({
   description: 'Mock Endpoint Response Data to Serve',
   fields: () => ({
     mimeType: {
-      type: GraphQLNonNull(mimeTypeEnum),
+      type: mimeTypeEnum,
       description: 'The MIME type of this response',
     },
     headers: {
@@ -80,7 +118,7 @@ const endpointType = new GraphQLObjectType({
       description: 'The route that the endpoint will handle',
     },
     method: {
-      type: GraphQLNonNull(GraphQLString),
+      type: GraphQLNonNull(httpMethodEnum),
       description: 'The HTTP method that the endpoint will handle',
     },
     response: {
@@ -90,4 +128,130 @@ const endpointType = new GraphQLObjectType({
   }),
 });
 
-const endpointsQueryType = new GraphQLQueryType({});
+const newEndpointResponseDataType = new graphql.GraphQLInputObjectType({
+  name: 'EndpointResponseData',
+  description: 'Mock Endpoint Response Data to Serve',
+  fields: () => ({
+    mimeType: {
+      type: mimeTypeEnum,
+      defaultValue: mimeTypeEnum.getValue('PLAIN').value,
+      description: 'The MIME type of this response',
+    },
+    headers: {
+      type: GraphQLList(GraphQLList(GraphQLString)),
+      description: 'A list of header data to send',
+    },
+    redirect: {
+      type: GraphQLString,
+      description: 'The redirect route that should be taken if status is 3xx',
+    },
+    body: {
+      type: GraphQLNonNull(GraphQLString),
+      description: 'The body data to send',
+    },
+    statusCode: {
+      type: GraphQLNonNull(GraphQLInt),
+      description: 'The response status code',
+    },
+  }),
+});
+
+const newEndpointDataType = new graphql.GraphQLInputObjectType({
+  name: 'EndpointData',
+  description: 'Data to create a mock endpoint',
+  fields: () => ({
+    route: {
+      type: GraphQLNonNull(GraphQLString),
+      description: 'The route that the endpoint will handle',
+    },
+    method: {
+      type: GraphQLNonNull(httpMethodEnum),
+      description: 'The HTTP method that the endpoint will handle',
+    },
+    response: {
+      type: GraphQLNonNull(newEndpointResponseDataType),
+      description: 'The response data to serve',
+    },
+  }),
+});
+
+const endpointsMutationType = new graphql.GraphQLObjectType({
+  name: 'EndpointMutation',
+  description: 'Mutation methods associated with an Endpoint Object',
+  fields: () => ({
+    createEndpoint: {
+      type: GraphQLNonNull(endpointType),
+      args: {
+        data: {
+          description: 'data of the endpoint',
+          type: GraphQLNonNull(newEndpointDataType),
+        },
+      },
+      resolve: (root, { data }) => root.addEndpoint(data),
+    },
+    replaceEndpoint: {
+      type: GraphQLNonNull(endpointType),
+      args: {
+        id: {
+          description: 'id of the endpoint to change',
+          type: GraphQLNonNull(GraphQLString),
+        },
+        data: {
+          description: 'data with which to update the endpoint',
+          type: GraphQLNonNull(newEndpointDataType),
+        },
+      },
+      resolve: (root, { id, data }) => root.replaceEndpoint(id, data),
+    },
+    deleteEndpoint: {
+      type: GraphQLNonNull(endpointType),
+      args: {
+        id: {
+          description: 'id of the endpoint to delete',
+          type: GraphQLNonNull(GraphQLString),
+        },
+      },
+      resolve: (root, { id }) => root.deleteEndpoint(id),
+    },
+  }),
+});
+
+const endpointsQueryType = new GraphQLObjectType({
+  name: 'EndpointQuery',
+  description: 'Query methods associated with an Endpoint Object',
+  fields: () => ({
+    endpoints: {
+      type: GraphQLList(endpointType),
+      resolve: root => root.endpoints,
+    },
+    endpoint: {
+      type: endpointType,
+      args: {
+        id: {
+          description: 'id of the endpoint',
+          type: GraphQLNonNull(GraphQLString),
+        },
+      },
+      resolve: (root, { id }) => root.getEndpoint(id),
+    },
+  }),
+});
+
+function setupGraphQLEndpointRoutes(app, route, store) {
+  const schema = new GraphQLSchema({
+    query: endpointsQueryType,
+    mutation: endpointsMutationType,
+    types: [endpointResponseType, endpointType, newEndpointDataType],
+  });
+
+  app.use(
+    route,
+    graphqlHTTP({
+      schema,
+      rootValue: store,
+      graphiql: true, // TODO: Use NODE_ENV to conditionally enable this
+    }),
+  );
+}
+
+module.exports = setupGraphQLEndpointRoutes;
